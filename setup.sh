6b0e1e018adbd558fed1779aea9e6e61b181c205
Huỳnh Minh Anh <122679072+minhanh55@users.noreply.github.com>
2024-03-19 18:17:13 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\033[0m\n\n'
+termux-setup-storage
+apt update
+apt upgrade
+printf '\n\033[1;32m Tiến Hành Cài Python\033[0m\n\n'
+pkg install python
+pip install requests
+curl -s https://raw.githubusercontent.com/minhanh55/tiktok/main/tiktok.py -o $home/usr/bin/ncngoc
+chmod 777 $home/usr/bin/ncngoc
+
+printf '\n\033[1;32m Gõ \033[1;33mncngoc \033[1;32mĐể Vào Tool \n\n'
